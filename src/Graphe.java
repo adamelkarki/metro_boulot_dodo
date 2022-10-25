2e869e331ff03300186989f33640081efdb8d629
mog4dor <adamkar94@gmail.com>
2022-10-25 21:08:32 +0200

    First Dikjstra implementation -> need to fix the branchements and terminus

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,13 +1,15 @@
+import javax.print.attribute.standard.PresentationDirection;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.TreeMap;
+import java.lang.reflect.Array;
+import java.util.*;
 
+import static java.util.Collections.replaceAll;
 import static java.util.Collections.sort;
 
 public class Graphe {
 
     private ArrayList<Sommet> sommets;
+    private Map<Integer, Integer> result = new HashMap<>();
     private ArrayList<Arete> aretes;
     private File file;
 
@@ -31,7 +33,9 @@ public class Graphe {
         this.sommets = sommets;
     }
 
-    public ArrayList<Arete> getAretes() { return this.aretes; }
+    public ArrayList<Arete> getAretes() {
+        return this.aretes;
+    }
 
 
     public void setAretes(ArrayList<Arete> aretes) {
@@ -56,18 +60,18 @@ public class Graphe {
         int tps = 0;
 
 
-        while((line = br.readLine()) != null) {
-            if(line.startsWith("V ")) {
+        while ((line = br.readLine()) != null) {
+            if (line.startsWith("V ")) {
                 line = line.substring(2);
-                branchement = Integer.parseInt(line.substring(line.length()-1));
-                line = line.substring(0,line.length()-1);
-                num_sommet = Integer.parseInt(line.substring(0,4));
+                branchement = Integer.parseInt(line.substring(line.length() - 1));
+                line = line.substring(0, line.length() - 1);
+                num_sommet = Integer.parseInt(line.substring(0, 4));
                 line = line.substring(5);
 
-                String [] regex = line.split(";");
+                String[] regex = line.split(";");
 
                 nom_station = regex[0];
-                numero_ligne = regex[1].substring(0,regex[1].length()-1);
+                numero_ligne = regex[1].substring(0, regex[1].length() - 1);
                 terminus = Boolean.parseBoolean(regex[2]);
 
                 //System.out.println(branchement);
@@ -78,12 +82,12 @@ public class Graphe {
                 //System.out.println(numero_ligne);
                 //System.out.println(terminus);
 
-                Sommet s = new Sommet(num_sommet,nom_station, numero_ligne, terminus, branchement);
+                Sommet s = new Sommet(num_sommet, nom_station, numero_ligne, terminus, branchement);
 //                System.out.println(s.toString());
                 this.sommets.add(s);
             }
-            if(line.startsWith("E ")) {
-                String [] regex = line.split(" ");
+            if (line.startsWith("E ")) {
+                String[] regex = line.split(" ");
 
                 sommetA = Integer.parseInt(regex[1]);
                 sommetB = Integer.parseInt(regex[2]);
@@ -105,10 +109,10 @@ public class Graphe {
         ArrayList<Integer> adjacents = new ArrayList<>();
         ArrayList<Sommet> sommetsAdjacents = new ArrayList<Sommet>();
         for (Arete a : this.getAretes()) {
-            if(a.getNum_sommet2() == s.getNum_sommet()) {
+            if (a.getNum_sommet2() == s.getNum_sommet()) {
                 adjacents.add(a.getNum_sommet1());
             }
-            if(a.getNum_sommet1() == s.getNum_sommet()) {
+            if (a.getNum_sommet1() == s.getNum_sommet()) {
                 adjacents.add(a.getNum_sommet2());
             }
         }
@@ -126,7 +130,7 @@ public class Graphe {
     public ArrayList<Arete> aretesAdjacentes(Sommet s) {
         ArrayList<Arete> aretesAdjacentes = new ArrayList<Arete>();
         for (Arete a : this.getAretes()) {
-            if(a.getNum_sommet2() == s.getNum_sommet() || a.getNum_sommet1() == s.getNum_sommet()) {
+            if (a.getNum_sommet2() == s.getNum_sommet() || a.getNum_sommet1() == s.getNum_sommet()) {
                 aretesAdjacentes.add(a);
             }
         }
@@ -136,11 +140,11 @@ public class Graphe {
 
     public boolean connexite(Sommet s1) {
         s1.setSommetVisite(true);
-        ArrayList<Sommet>  sommetsAdjacents = sommetsAdjacencents(s1);
+        ArrayList<Sommet> sommetsAdjacents = sommetsAdjacencents(s1);
         //System.out.println(sommetsAdjacents);
 
-        for(Sommet next : sommetsAdjacents) {
-            if(next.isSommetVisite() == false){
+        for (Sommet next : sommetsAdjacents) {
+            if (next.isSommetVisite() == false) {
                 connexite(next);
             }
         }
@@ -149,20 +153,58 @@ public class Graphe {
 
     public int kruskal() {
         Collections.sort(this.aretes);
-        TreeMap<Integer,Integer> sommetsVisites = new TreeMap<>();
+        TreeMap<Integer, Integer> sommetsVisites = new TreeMap<>();
         int acpm = 0;
 
-        for(Arete arete : this.getAretes()) {
-            if((!sommetsVisites.containsKey(arete.getNum_sommet1()) && !sommetsVisites.containsValue(arete.getNum_sommet2())) || (!sommetsVisites.containsKey(arete.getNum_sommet2()) && !sommetsVisites.containsValue(arete.getNum_sommet1()))) {
-                sommetsVisites.put(arete.getNum_sommet1(),arete.getNum_sommet2());
+        for (Arete arete : this.getAretes()) {
+            if ((!sommetsVisites.containsKey(arete.getNum_sommet1()) && !sommetsVisites.containsValue(arete.getNum_sommet2())) || (!sommetsVisites.containsKey(arete.getNum_sommet2()) && !sommetsVisites.containsValue(arete.getNum_sommet1()))) {
+                sommetsVisites.put(arete.getNum_sommet1(), arete.getNum_sommet2());
                 acpm += arete.getTps();
             }
         }
         return acpm;
     }
 
-    public TreeMap<Sommet, Integer> dikjstra(Sommet sommetSource, Sommet destination) {
+    public Map<Integer, Integer> dikjstra(Sommet sommetSource, Sommet destination) {
+        int sommetAdjacent = 0;
+        sommetSource.setSommetVisite(true);
+        ArrayList<Sommet> voisins = sommetsAdjacencents(sommetSource);
+        ArrayList<Arete> voisines = aretesAdjacentes(sommetSource);
 
+        //On retourne result lorsque les deux sommets sont voisins
+        for (Arete arete : voisines) {
+            if (arete.getNum_sommet1() == sommetSource.getNum_sommet()) {
+                sommetAdjacent = arete.getNum_sommet2();
+                this.result.put(sommetAdjacent, arete.getTps());
+            }
+            if (arete.getNum_sommet2() == sommetSource.getNum_sommet()) {
+                sommetAdjacent = arete.getNum_sommet1();
+                this.result.put(sommetAdjacent, arete.getTps());
+            }
+            if ((arete.getNum_sommet1() == destination.getNum_sommet() || arete.getNum_sommet2() == destination.getNum_sommet())) {
+                System.out.println("DERNIER TOUR -> ILS SONT VOISINS.");
+                this.result.put(destination.getNum_sommet(), arete.getTps());
+                return this.result;
+            }
+        }
+        Set<Integer> set_iterator = this.result.keySet();
+        ArrayList<Sommet> sommetsFinaux = new ArrayList<>();
+        Iterator<Integer> it = set_iterator.iterator();
+
+        if (!this.result.containsKey(destination.getNum_sommet())) {
+            while (it.hasNext()) {
+                int i = it.next();
+                for (Sommet sommet : voisins) {
+                    if (sommet.getNum_sommet() == i && !sommet.isSommetVisite()) {
+                        sommetsFinaux.add(sommet);
+                    }
+                }
+            }
+            for (Sommet sommet : sommetsFinaux) {
+                dikjstra(sommet, destination);
+            }
+        }
+        return this.result;
     }
-
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.omg.PortableServer.POA;
+
 import javax.sound.sampled.Port;
 import java.io.IOException;
 
@@ -13,12 +15,17 @@ public class Main {
         Sommet Mairie_DIvry = g.getSommets().get(179);
         Sommet Porte_DIvry = g.getSommets().get(261);
         Sommet PorteDeChoisy = g.getSommets().get(266);
+        Sommet PorteDitalie = g.getSommets().get(260);
+        Sommet PlaceDItalie = g.getSommets().get(244);
 //        System.out.println(g.getAretes());
 //        System.out.println(g.sommetsAdjacencents(Porte_DIvry));
 //        g.aretesAdjacentes(Porte_DIvry);
 //        g.connexite(Porte_DIvry);
-//        System.out.println(g.kruskal());
-        g.dikjstra(Porte_DIvry,PierreEtMarieCurie);
+////        System.out.println(g.kruskal());
+//        System.out.println(g.sommetsAdjacencents(PorteDeChoisy));
+//        System.out.println(g.sommetsAdjacencents(s1));
+//        System.out.println(g.sommetsAdjacencents(PierreEtMarieCurie));
+        System.out.println(g.dikjstra(Mairie_DIvry,PlaceDItalie));
 
 
 
